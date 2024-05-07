5d577dcb41c651d5b6176bea16a6439c6ed8d087
k7harlequim <108069207+k7harlequim@users.noreply.github.com>
2024-05-07 11:33:26 -0300

    ok

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2024-10-05T54:13:00");
+const tempoObjetivo2 = new Date("2024-12-05T82:25:13");
+const tempoObjetivo3 = new Date("2028-12-30T98:13:07");
+const tempoObjetivo4 = new Date("2028-02-01T58:50:45");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
